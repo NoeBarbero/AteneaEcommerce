3776e475abbc622ed430114e1608aab0b789e4f6
NoeBarbero <marianoelbarbero@live.com>
2022-10-01 16:11:16 -0300

    actualizzo array

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* lista de productos */
 let stockProductos = [
     {id: 1, nombre: "Peeling", tipo: "Peeling", cantidad: 1, desc: "Exfoliación de las capas más superficiales de la piel para favorecer su sustitución por otras de mejor calidad y textura. Se induce así la creación de nuevas capas de la dermis y la epidermis.", precio: 2200, img: './img/peeling.jpg'},
     {id: 2, nombre: "Limpieza facial con aparatología", tipo: "Limpieza", cantidad: 1, desc: "Limpieza profunda con hidratación en la que se utiliza la radiofrecuencia facial más avanzada. Se aplican tres exfoliantes distintos, un serum, una mascarilla y se finaliza con un crema facial.", precio: 2500, img: './img/limpiezafacial_aparato.webp'},
@@ -6,21 +5,23 @@ let stockProductos = [
     {id: 4, nombre: "Microdermoabrasion", tipo: "Peeling", cantidad: 1, desc: "Tratamiento de medicina estética que consiste en aplicar unos granos diminutos que consiguen exfoliar y eliminar las células muertas de la piel. Esto hace que la piel se renueve y aparezca una nueva más bella, cuidada y saneada.", precio: 2500, img: './img/microdermoabrasion.jpg'},
     {id: 5, nombre: "Masajes reductores", tipo: "Masajes", cantidad: 1, desc: "Método para combatir y eliminar la acumulación de grasa de una zona localizada del cuerpo, como glúteos o abdomen. Además, favorece la expulsión de toxinas, beneficia el flujo sanguíneo y mejora el sistema digesitvo, entre otras ventajas.", precio: 3200, img: './img/masajesreductores.jpg'},
     {id: 6, nombre: "Lifting de pestañas", tipo: "Pestañas", cantidad: 1, desc: "Tratamiento que alarga y crea una ligera curva hacia arriba de manera natural y duradera, consiguiendo mayor longitud y espesor.", precio: 3500, img: './img/rostro_flores.webp'},
-    {id: 6, nombre: "Limpieza facial profunda", tipo: "Limpieza", cantidad: 1, desc: "Oxigena tu piel, retrasa la aparición de arrugas, piel más luminosa, elimina y previene el acné y los puntos negros", precio: 2100, img: './img/limpieza_facial.webp'},
+    {id: 7, nombre: "Limpieza facial profunda", tipo: "Limpieza", cantidad: 1, desc: "Oxigena tu piel, retrasa la aparición de arrugas, piel más luminosa, elimina y previene el acné y los puntos negros", precio: 2100, img: './img/limpieza_facial.webp'},
 ];
+
 
 const contenedorProductos = document.getElementById('contenedor-productos')
 
+//TERCER PASO
+
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-
+//SEXTO PASO
 const botonVaciar = document.getElementById('vaciar-carrito')
-
+//SEXTIMO PASO, MODIFICAR LOS CONTADORES
 const contadorCarrito = document.getElementById('contadorCarrito')
 
+//OCTAVO PASO
 const cantidad = document.getElementById('cantidad')
-
 const precioTotal = document.getElementById('precioTotal')
-
 const cantidadTotal = document.getElementById('cantidadTotal')
 
 let carrito = []
@@ -31,13 +32,13 @@ document.addEventListener('DOMContentLoaded', () => {
         actualizarCarrito()
     }
 })
-
+//SEXTO PASO
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
 })
 
-//INYECTAR EL HTML
+//PRIMER PRIMER PASO, INYECTAR EL HTML
 stockProductos.forEach((producto) => {
     const div = document.createElement('div')
     div.classList.add('producto')
@@ -45,49 +46,75 @@ stockProductos.forEach((producto) => {
     <img src=${producto.img} alt= "">
     <h3>${producto.nombre}</h3>
     <p>${producto.desc}</p>
+    <p>Talle: ${producto.talle}</p>
     <p class="precioProducto">Precio:$ ${producto.precio}</p>
     <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
     `
     contenedorProductos.appendChild(div)
 
+    //2 - SEGUNDO PASO, LUEGO DE QUE INSERTEMOS EL HTML EN EL DOM:
     const boton = document.getElementById(`agregar${producto.id}`)
+    //Por cada elemento de mi array, creo un div, lo cuelgo, le pongo un id particular, una vez colgado
+    //le hago un get element by id (el de agregar) Obtengo el elemento y a dicho elemento le agregamos
+    //el add event listener
 
     boton.addEventListener('click', () => {
+        //esta funcion ejecuta el agregar el carrito con la id del producto
         agregarAlCarrito(producto.id)
+        //
     })
 })
 
+// 1- PRIMER PASO
 
+//AGREGAR AL CARRITO
 const agregarAlCarrito = (prodId) => {
 
-    const existe = carrito.some (prod => prod.id === prodId)
+    //PARA AUMENTAR LA CANTIDAD Y QUE NO SE REPITA
+    const existe = carrito.some (prod => prod.id === prodId) //comprobar si el elemento ya existe en el carro
 
-    if (existe){
-        const prod = carrito.map (prod => {
+    if (existe){ //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
+        const prod = carrito.map (prod => { //creamos un nuevo arreglo e iteramos sobre cada curso y cuando
+            // map encuentre cual es el q igual al que está agregado, le suma la cantidad
             if (prod.id === prodId){
                 prod.cantidad++
             }
         })
-    } else {
-        const item = stockProductos.find((prod) => prod.id === prodId)
+    } else { //EN CASO DE QUE NO ESTÉ, AGREGAMOS EL CURSO AL CARRITO
+        const item = stockProductos.find((prod) => prod.id === prodId)//Trabajamos con las ID
+        //Una vez obtenida la ID, lo que haremos es hacerle un push para agregarlo al carrito
         carrito.push(item)
     }
-
-    actualizarCarrito()
+    //Va a buscar el item, agregarlo al carrito y llama a la funcion actualizarCarrito, que recorre
+    //el carrito y se ve.
+    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
+    //MODIFICA EL CARRITO
 }
+//agregarAlCarrito(1) //Le pasamos el ID por parametro. Tenemos que asigarle como evento esta funcion al boton
+//con el id de su producto correspondiente
 
+// 5 - QUINTO PASO
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
 
-    const indice = carrito.indexOf(item)
+    const indice = carrito.indexOf(item) //Busca el elemento q yo le pase y nos devuelve su indice.
 
-    carrito.splice(indice, 1) 
-    actualizarCarrito()
+    carrito.splice(indice, 1) //Le pasamos el indice de mi elemento ITEM y borramos 
+    // un elemento 
+    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
+    //MODIFICA EL CARRITO
     console.log(carrito)
 }
 
 const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = "" 
+    //4- CUARTO PASO
+    //LOS APPENDS SE VAN ACUMULANDO CON LO QE HABIA ANTES
+    contenedorCarrito.innerHTML = "" //Cada vez que yo llame a actualizarCarrito, lo primero q hago
+    //es borrar el nodo. Y despues recorro el array lo actualizo de nuevo y lo rellena con la info
+    //actualizado
+    //3 - TERCER PASO. AGREGAR AL MODAL. Recorremos sobre el array de carrito.
+
+    //Por cada producto creamos un div con esta estructura y le hacemos un append al contenedorCarrito (el modal)
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
@@ -103,30 +130,12 @@ const actualizarCarrito = () => {
         localStorage.setItem('carrito', JSON.stringify(carrito))
 
     })
-    
-    contadorCarrito.innerText = carrito.length
-    
+    //SEPTIMO PASO
+    contadorCarrito.innerText = carrito.length // actualizamos con la longitud del carrito.
+    //OCTAVO PASO
     console.log(carrito)
     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+    //Por cada producto q recorro en mi carrito, al acumulador le suma la propiedad precio, con el acumulador
+    //empezando en 0.
+
 }
-
-const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
-const botonAbrir = document.getElementById('boton-carrito')
-const botonCerrar = document.getElementById('carritoCerrar')
-const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
-
-
-botonAbrir.addEventListener('click', ()=>{
-    contenedorModal.classList.toggle('modal-active')
-})
-botonCerrar.addEventListener('click', ()=>{
-    contenedorModal.classList.toggle('modal-active')
-})
-
-contenedorModal.addEventListener('click', (event) =>{
-    contenedorModal.classList.toggle('modal-active')
-
-})
-modalCarrito.addEventListener('click', (event) => {
-    event.stopPropagation() 
-})
